d3ef88721ec40068792fbaca97553c8b87f315b8
Brett Boettcher <hgdxg@BrettsRTX>
2025-10-19 01:29:55 -0600

    added routing to departments list to take you to the timesheet page

--- src/app/components/departments/departments.ts
+++ src/app/components/departments/departments.ts
@@ -1,8 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { DepartmentInterface } from '../../interfaces/department';
 import { DepartmentsService } from '../../services/departments.service';
-import { MatCard } from "@angular/material/card";
-import { MaterialModule } from "../../modules/material-module";
+import { MatCard } from '@angular/material/card';
+import { MaterialModule } from '../../modules/material-module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-departments',
@@ -11,10 +12,16 @@ import { MaterialModule } from "../../modules/material-module";
   styleUrl: './departments.scss',
 })
 export class Departments implements OnInit {
-  departments!: DepartmentInterface[];
   private departmentsService = inject(DepartmentsService);
+  private router = inject(Router);
+
+  departments!: DepartmentInterface[];
 
   ngOnInit() {
     this.departments = this.departmentsService.departments;
+  }
+
+  goToDepartment(departmentId: string) {
+    this.router.navigate(['timesheet', { id: departmentId }]);
   }
 }
