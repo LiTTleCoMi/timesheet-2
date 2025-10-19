88179c0b7e28971a9d4a5c44c685e42f766466f4
Brett Boettcher <hgdxg@BrettsRTX>
2025-10-19 00:56:43 -0600

    added routing to the navbar

--- src/app/components/top-navbar/top-navbar.ts
+++ src/app/components/top-navbar/top-navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../modules/material-module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-top-navbar',
-  imports: [MaterialModule],
+  imports: [MaterialModule, RouterModule],
   templateUrl: './top-navbar.html',
   styleUrl: './top-navbar.scss'
 })
