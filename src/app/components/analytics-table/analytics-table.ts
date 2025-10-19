3521b57cc8b918187446ab9e3f2ba89d3d9c4019
Brett Boettcher <hgdxg@BrettsRTX>
2025-10-19 00:51:21 -0600

    completed unit #3

---
+++ src/app/components/analytics-table/analytics-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics-table',
+  imports: [],
+  templateUrl: './analytics-table.html',
+  styleUrl: './analytics-table.scss'
+})
+export class AnalyticsTable {
+
+}
---
+++ src/app/components/top-navbar/top-navbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../modules/material-module';
+
+@Component({
+  selector: 'app-top-navbar',
+  imports: [MaterialModule],
+  templateUrl: './top-navbar.html',
+  styleUrl: './top-navbar.scss'
+})
+export class TopNavbar {
+
+}
