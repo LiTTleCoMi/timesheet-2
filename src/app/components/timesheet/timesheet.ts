c843619a15b28fc977c1035e64c4a9947451dd0d
Brett Boettcher <hgdxg@BrettsRTX>
2025-10-19 02:17:28 -0600

    timesheet now fetches a department based on url params and displays name

--- src/app/components/timesheet/timesheet.ts
+++ src/app/components/timesheet/timesheet.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DepartmentsService } from '../../services/departments.service';
+import { DepartmentInterface } from '../../interfaces/department';
 
 @Component({
   selector: 'app-timesheet',
   imports: [],
   templateUrl: './timesheet.html',
-  styleUrl: './timesheet.scss'
+  styleUrl: './timesheet.scss',
 })
-export class Timesheet {
+export class Timesheet implements OnInit {
+  private route = inject(ActivatedRoute);
+  private departmentsService = inject(DepartmentsService);
 
+  departments!: DepartmentInterface[];
+  department!: DepartmentInterface | undefined;
+
+  ngOnInit() {
+    this.departments = this.departmentsService.departments;
+    this.department = this.departments.find(
+      (department) => department.id === this.route.snapshot.params['id']
+    );
+  }
 }
